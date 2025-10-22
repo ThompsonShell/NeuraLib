cb5ba63969ab5d58866bdc666f22fa4ce389bf1a
ThompsonShell <asilbek.rajabov.official@gmail.com>
2025-10-23 00:24:45 +0500

    order model added

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.conf import settings
+
+from apps.utils.models import AbstarBaseModel
+
 
 # Create your models here.
+class Order(AbstarBaseModel):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    total_price = models.DecimalField(max_digits=10,
+                                      decimal_places=2,
+                                      default=0)
+    store_name = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
+    paid_at = models.DateTimeField(null=True,
+                                   blank=True)
+    delivery_price = models.DecimalField(max_digits=10,
+                                decimal_places=2,
+                                default=0)
+    coupon = models.ForeignKey(to='Coupon', on_delete=models.PROTECT)
+
+
+    def __str__(self):
+        return f"user {self.user.id}"
+
+
+class OrderProduct(AbstarBaseModel):
+    quantity = models.IntegerField()
+    order = models.ForeignKey(to='Order', on_delete=models.PROTECT)
+    book = models.ForeignKey(to="Book", on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2,
+                                default=0)
+
+
+
+
