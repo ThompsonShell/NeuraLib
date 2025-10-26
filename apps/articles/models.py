b02f82df1345abc62011351e0d50c4b707f5e320
ThompsonShell <asilbek.rajabov.official@gmail.com>
2025-10-26 22:24:19 +0500

    gitignore file changed for .venv

--- apps/articles/models.py
+++ apps/articles/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
+from django.db.models import PROTECT
 
-# Create your models here.
+
+author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=PROTECT)
+title = models.CharField(max_length=500)
+text = models.TextField(max_length=100_000_000)
+comment = models.CharField(max_length=10000)
+image = models.FileField(upload_to='article/image')
+published_at = models.DateTimeField()
+
+
+
+
+
